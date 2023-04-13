21d35c9d3e4c4df3c1efd5c2e7c9608811aea419
Parkeunji00 <ebeg5661@naver.com>
2023-04-13 10:17:27 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/callor/hello/Hellokorea.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+
+public class Hellokorea {
+    public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+	}
+}
+
