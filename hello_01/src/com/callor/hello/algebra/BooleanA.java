5b154f02636d38318ae507913381340edf18fae4
Parkeunji00 <ebeg5661@naver.com>
2023-04-14 09:16:59 +0900

    2023-04-14-수업시작

--- hello_01/src/com/callor/hello/algebra/BooleanA.java
+++ hello_01/src/com/callor/hello/algebra/BooleanA.java
@@ -1,5 +1,15 @@
-package com.callor.hello.algebra;
+package com.callor.hello.algebra; 
 
 public class BooleanA {
 
+	public static void main(String[] args) {
+		System.out.println(30 > 40);
+		System.out.println(30 < 40);
+		System.out.println(30 > 30);
+		System.out.println(30 >= 30);
+		System.out.println(30 <= 30);
+		System.out.println(30 == 30);
+	    
+			}		
+	
 }
