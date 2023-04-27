7c3d3ca4f6ee3ad5bee358bf94a32c93d1e45b99
Parkeunji00 <ebeg5661@naver.com>
2023-04-27 16:21:32 +0900

    0427

--- Java_OneDay_001_Score/src/com/callor/score/HomeworkC.java
+++ Java_OneDay_001_Score/src/com/callor/score/HomeworkC.java
@@ -7,7 +7,7 @@ public class HomeworkC {
 	public static void main(String[] args) {
 
 		int[] names = new int[10];
-		
+
 		ScoreDto[] scores = new ScoreDto[names.length];
 
 		for (int index = 0; index < scores.length; index++) {
@@ -26,31 +26,47 @@ public class HomeworkC {
 			scores[index].scMath = scServiceA.getScore();
 			scores[index].scMusic = scServiceA.getScore();
 			scores[index].scArt = scServiceA.getScore();
-			
-		
+
 		}
 		for (int i = 0; i < scores.length; i++) {
 			scServiceA.scorePrint(scores[i]);
-			 
-			
+
 		}
 		int KorTotal = 0;
 		int EngTotal = 0;
 		int MathTotal = 0;
 		int MusicTotal = 0;
 		int ArtTotal = 0;
-		int TotalSum = 0;
-		double Avg = 0.0;
+
 		for (int i = 0; i < scores.length; i++) {
-		
-			
+			KorTotal += scores[i].scKor;
+			EngTotal += scores[i].scEng;
+			MathTotal += scores[i].scMath;
+			MusicTotal += scores[i].scMusic;
+			ArtTotal += scores[i].scArt;
+
 		}
-	
+		int Total = KorTotal + EngTotal + MathTotal + MusicTotal + ArtTotal;
+
 		System.out.println("-".repeat(70));
-		System.out.println("총점");
+		System.out.print("총점");
+		System.out.printf("%11d", KorTotal);
+		System.out.printf("%8d", EngTotal);
+		System.out.printf("%8d", MathTotal);
+		System.out.printf("%8d", MusicTotal);
+		System.out.printf("%8d", ArtTotal);
+		System.out.printf("%8d", Total);
+
 		System.out.println();
-		System.out.println("평균");
+		System.out.print("평균");
+		System.out.printf("%8.2f", (float) KorTotal / scores.length);
+		System.out.printf("%8.2f", (float) EngTotal / scores.length);
+		System.out.printf("%8.2f", (float) MathTotal / scores.length);
+		System.out.printf("%8.2f", (float) MusicTotal / scores.length);
+		System.out.printf("%8.2f", (float) ArtTotal / scores.length);
+		System.out.printf("%20.2f", (float) Total / scores.length);
+		System.out.println();
 		System.out.println("=".repeat(70));
-		
+
 	}
 }
