f6e492d5d84d422fe98fee0f81c25342d8dcfdc6
Parkeunji00 <ebeg5661@naver.com>
2023-05-03 16:37:08 +0900

    0503

--- Java_012_Scanner/src/com/callor/system/service/StudentServiceV2.java
+++ Java_012_Scanner/src/com/callor/system/service/StudentServiceV2.java
@@ -21,15 +21,7 @@ public class StudentServiceV2 {
 			System.out.println("학생정보를 입력하세요");
 			System.out.println("학번 >>");
 			String stNum = scan.nextLine();
-			.setStNum(stNum);
-			
-			System.out.println("이름 >>");
-			String stName = scan.nextLine();
-			Std.setStName(stName);
-			
-			System.out.println("학과 >>");
-			String stDept = scan.nextLine();
-			Std.setStDept(stDept);
+	
 			
 			System.out.println("학년 >>");
 			String stGrade = scan.nextLine();
@@ -38,12 +30,12 @@ public class StudentServiceV2 {
 	}
 
 	public void printStudents() {
-		for (int i = 0; i < student.length; i++) {
-			String[] name1 = new String[];
+		
+		
+			
 			System.out.printf("=".repeat(70));
 			System.out.printf("학번\t이름\t학과\t학년\t전화번호");
 			System.out.println();
 
 		}
 	}
-}
