80627d9a3378e9204007b4adc92deb756d88f58b
Parkeunji00 <ebeg5661@naver.com>
2023-05-08 16:41:57 +0900

    0503

---
+++ Java_015_Class/src/com/callor/classes/ExecB.java
@@ -0,0 +1,9 @@
+package com.callor.classes;
+
+public class ExecB {
+	public static void main(String[] args) {
+		ClassD classD = new ClassD();
+		classD.printList();
+	}
+
+}
---
+++ Java_015_Class/src/com/callor/classes/ExecE.java
@@ -0,0 +1,16 @@
+package com.callor.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.callor.classes.models.AnimalDto;
+
+public class ExecE {
+	public static void main(String[] args) {
+		List<AnimalDto> anm = new ArrayList();
+	
+		
+		
+		
+	}
+}
